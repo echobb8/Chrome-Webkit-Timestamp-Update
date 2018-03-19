f4d020b9a261125ac2c245747de3b5f15ab8e8eb
echobb8 <davidjrojas@gmail.com>
2018-03-19 15:52:34 -0400

    Added comments
    Updated script with comments.

--- dbedit.py
+++ dbedit.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 import sqlite3
 
-sqlite_file = 'History'
-table_name = 'downloads'
-start_time_column = 'start_time'
-end_time_column = 'end_time'
+sqlite_file = 'History' # path to the sqlite Chrome History database file.
+table_name = 'downloads' # the table you want to manipulate, in this case the downloads history.
+start_time_column = 'start_time' # first column you want to edit with the Chrome/webkit formatted timestamps.
+end_time_column = 'end_time' # this is an additional column you may want to edit in the downloads table.
+# you can always add more columns that contain timestamp data and modify the code below.
 
 # Connecting to the database file
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
+# creating a variable to hold the converted timestamps for each of the above defined columns
 start_time = "SELECT datetime((start_time/1000000)-11644473600, 'unixepoch', 'localtime')"
 end_time = "SELECT datetime((end_time/1000000)-11644473600, 'unixepoch', 'localtime')"
 
@@ -21,5 +23,6 @@ c.execute("UPDATE {tn} SET {st}=({dt})".\
 c.execute("UPDATE {tn} SET {et}=({dt})".\
     format(tn=table_name, et=end_time_column, dt=end_time))
 
+# Commit changes and close the connection.
 conn.commit()
 conn.close()
